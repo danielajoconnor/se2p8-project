e7b8b031309966386c5e57e00202a6231bfd2cfa
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-03-01 19:28:17 +0000

    added footer component

---
+++ components/home/footer.js
@@ -0,0 +1,53 @@
+export default function Footer() {
+  return (
+    <footer class="bg-neutral-300 text-center text-white dark:bg-green-700 dark:text-neutral-200">
+      <div class="container p-6">
+        <div class="grid gap-4 md:grid-cols-3 lg:grid-cols-6">
+          <div class="mb-6 lg:mb-0">
+            <img
+              src="https://tecdn.b-cdn.net/img/new/fluid/city/113.webp"
+              class="w-full rounded-md shadow-lg"
+            />
+          </div>
+          <div class="mb-6 lg:mb-0">
+            <img
+              src="https://tecdn.b-cdn.net/img/new/fluid/city/111.webp"
+              class="w-full rounded-md shadow-lg"
+            />
+          </div>
+          <div class="mb-6 lg:mb-0">
+            <img
+              src="https://tecdn.b-cdn.net/img/new/fluid/city/112.webp"
+              class="w-full rounded-md shadow-lg"
+            />
+          </div>
+          <div class="mb-6 lg:mb-0">
+            <img
+              src="https://tecdn.b-cdn.net/img/new/fluid/city/114.webp"
+              class="w-full rounded-md shadow-lg"
+            />
+          </div>
+          <div class="mb-6 lg:mb-0">
+            <img
+              src="https://tecdn.b-cdn.net/img/new/fluid/city/115.webp"
+              class="w-full rounded-md shadow-lg"
+            />
+          </div>
+          <div class="mb-6 lg:mb-0">
+            <img
+              src="https://tecdn.b-cdn.net/img/new/fluid/city/116.webp"
+              class="w-full rounded-md shadow-lg"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div class="bg-green-700 p-4 text-center text-neutral-700 dark:bg-neutral-700 dark:text-neutral-200">
+        © 2023 Copyright:
+        <a class="dark:text-neutral-400" href="https://tailwind-elements.com/">
+          Tailwind Elements
+        </a>
+      </div>
+    </footer>
+  );
+}
