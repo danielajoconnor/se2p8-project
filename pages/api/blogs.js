ab4b1e37096787c6c5220f2c574f321dd1ed2a65
Daniel O'Connor <danielajoconnor@gmail.com>
2023-03-05 15:29:34 +0000

    added sql to blogs api

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -1,6 +1,9 @@
 import postgres from "postgres";
 import sql from "@/utils/postgres";
 
-export default function Handler(req, res) {
-
+export default async function Handler(req, res) {
+    const blogs = await sql`
+    select * from blogs
+    `
+    res.json(blogs);
 }
