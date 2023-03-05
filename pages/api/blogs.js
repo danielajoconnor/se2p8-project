5fc77a8a02e22c73b936c91db1398be3f00b0926
Daniel O'Connor <danielajoconnor@gmail.com>
2023-03-05 17:24:58 +0000

    created a component to display blogs in database on blogs page

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -1,9 +1,9 @@
 import postgres from "postgres";
 import sql from "@/utils/postgres";
 
-export default async function Handler(req, res) {
+export default async function handler(req, res) {
     const blogs = await sql`
     select * from blogs
     `
-    res.json(blogs);
+    res.json({blogs : blogs});
 }
--- pages/blogs.js
+++ pages/blogs.js
@@ -1,15 +1,16 @@
 import NavBar from "@/components/shared/navbar";
 import Footer from "@/components/shared/footer";
 import Content from "@/components/shared/content";
+import ListOfBlogs from "@/components/blogs/list-of-blogs";
 
 export default function Blog() {
-  return (
+  return( 
     <div>
       
-      <Content>
-        
-      </Content>
-      
-    </div>
-  );
+    <Content>
+      <ListOfBlogs />
+    </Content>
+    
+  </div>
+  )
 }
