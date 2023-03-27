90807a0ac81d6d662719e9131d7713c21be10cc7
Daniel O'Connor <danielajoconnor@gmail.com>
2023-03-27 21:07:52 +0100

    added loading bars to blogs and news

--- components/blogs/list-of-blogs/index.js
+++ components/blogs/list-of-blogs/index.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import SubTitle from "@/components/shared/title/subtitle";
+import Content from "@/components/shared/content";
 
 export default function ListOfBlogs() {
   const [blogs, setBlogs] = useState([]);
@@ -17,6 +18,24 @@ export default function ListOfBlogs() {
     setBlogs(blogs);
   };
 
+
+  if (!blogs || !blogs.length) {
+    return (
+      <div>
+        <div className="w-full text-center mx-auto bg-nature5">
+          <h1 className="py-[31px] md:py-[63px] font-serif text-3xl md:text-6xl tracking-tight text-nature1">
+            Blogs
+          </h1>
+        </div>
+
+        <div className="sm:grid-cols-1 md:grid-cols-2 text-center px-24 mx-auto text-base md:text-2xl py-16 text-nature1 tracking-tight bg-nature4">
+              <p className="animate-pulse">...</p>
+          </div>
+
+      </div>
+    );
+  }
+
   return (
     <div>
       <div className="w-full text-center mx-auto bg-nature5">
@@ -26,16 +45,15 @@ export default function ListOfBlogs() {
       </div>
       <ul className="flex flex-wrap text-center">
         {blogs.map((blog, id) => (
-          
-            <li
-              key={id}
-              className="text-nature1 py-4 odd:bg-nature3 even:bg-nature4 grow w-full md:w-[256px] list-none"
-            >
-              <SubTitle className="hover:underline-offset-6 "><a href={`/blogs/${blog.id}`}>{blog.title}</a></SubTitle>
-              <h3>{blog.author}</h3>
-              
-            </li>
-          
+          <li
+            key={id}
+            className="text-nature1 py-4 odd:bg-nature3 even:bg-nature4 grow w-full md:w-[256px] list-none"
+          >
+            <SubTitle className="hover:underline-offset-6 ">
+              <a href={`/blogs/${blog.id}`}>{blog.title}</a>
+            </SubTitle>
+            <h3>{blog.author}</h3>
+          </li>
         ))}
       </ul>
     </div>
--- components/news/index.js
+++ components/news/index.js
@@ -10,25 +10,45 @@ export default function News() {
   }, []);
 
   const getNews = async () => {
-        const response = await fetch(`/api/news-api`);
-        const data = await response.json();
+    const response = await fetch(`/api/news-api`);
+    const data = await response.json();
 
     const { articles } = data;
 
     setNews(articles);
   };
 
+  if (!articles.length) {
+    return (
+      <div className="flex flex-wrap text-center">
+        <div className="w-full text-center mx-auto bg-nature5">
+          <h1 className="py-[31px] md:py-[63px] font-serif text-3xl md:text-6xl tracking-tight text-nature1">
+            News
+          </h1>
+        </div>
+        <div className="w-full sm:grid-cols-1 md:grid-cols-2 text-center px-24 mx-auto text-base md:text-2xl py-16 text-nature1 tracking-tight bg-nature4">
+              <p className="animate-pulse">...</p>
+          </div>
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-wrap text-center">
-            <div className="w-full text-center mx-auto bg-nature5">
+      <div className="w-full text-center mx-auto bg-nature5">
         <h1 className="py-[31px] md:py-[63px] font-serif text-3xl md:text-6xl tracking-tight text-nature1">
           News
         </h1>
       </div>
       <ul className="w-full">
         {articles.map((article) => (
-          <li key={article.url} className="text-nature1 py-4 odd:bg-nature3 even:bg-nature4  w-full list-none font-serif">
-            <a className="text-xl underline" href={article.url}>{article.title}</a>
+          <li
+            key={article.url}
+            className="text-nature1 py-4 odd:bg-nature3 even:bg-nature4  w-full list-none font-serif"
+          >
+            <a className="text-xl underline" href={article.url}>
+              {article.title}
+            </a>
             <h2 className="text-sm">{article.author}</h2>
             <p>{article.description}</p>
           </li>
